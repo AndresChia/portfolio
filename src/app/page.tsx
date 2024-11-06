139066eeeb4fe128fa6de7740273ae70d7fba0d7
Andres_Chia <achia@soytul.com>
2024-11-06 16:20:03 -0500

    feat: 🎸 add principal components

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/footer";
+import Header from "@/components/Header";
+import Intro from "@/components/Intro";
+import Presentation from "@/components/Presentation";
+import I18nIntlProvider from "@/config/i18n/i18n-intl-provider";
+
+export default function Home() {
+  return (
+    <I18nIntlProvider>
+      <div className="font-[family-name:var(--font-geist-sans)] relative text-white bg-primary_1">
+        <Header />
+        <Presentation />
+        <Intro />
+        <Footer />
+        <div className="fixed z-[9999] top-[-300%] left-[-150%] h-[600%] w-[600%] animate-grain bg-[url(/noise.png)] bg-[200px] bg-repeat opacity-25 pointer-events-none"></div>
+      </div>
+    </I18nIntlProvider>
+  );
+}
---
+++ src/components/Presentation.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useScroll, useTransform, motion } from "framer-motion";
+
+import { useRef } from "react";
+
+export default function Presentation() {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], ["0vh", "150vh"]);
+
+  return (
+    <section
+      ref={container}
+      className="flex flex-col items-center justify-center h-screen px-8"
+      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+    >
+      <motion.div style={{ y }} className="relative h-full top-[220px]">
+        <div className="flex flex-col items-center justify-center text-center h-screen-half">
+          <h1 className="text-5xl sm:text-6xl lg:text-9xl">Andres Chia</h1>
+          <h2 className="font-light text-4xl sm:text-5xl lg:text-8xl">
+            Senior Developer
+          </h2>
+        </div>
+      </motion.div>
+    </section>
+  );
+}
